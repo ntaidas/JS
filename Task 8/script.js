07e7ced3291b9581a6510ec602a1ca0ce8ca978f
AidasD <ntaidas@gmail.com>
2023-10-27 11:53:56 +0300

    uzduotis 8 baigta

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    sum(a,b){
+        return a+b
+    }
+    subtraction(a,b){
+        return a-b
+    }
+    multiplication(a,b){
+        return a * b
+    }
+    division(a,b){
+        return a / b
+    }
+}
+
+const calc = new Calculator
+console.log(calc.sum(1,5))
+console.log(calc.subtraction(10,5))
+console.log(calc.multiplication(4,2))
+console.log(calc.division(6,2))
