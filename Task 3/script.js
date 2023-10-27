e882d8dd8d2bbe90263198384551f3021b7e66d0
AidasD <ntaidas@gmail.com>
2023-10-27 09:47:28 +0300

    uzduotis 3 baigta, keletas formatavimo pakeitimu 2 uzduotyje

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,33 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+/// pagriebiame mygtuka ir pridedam jam funkcionaluma
+document.querySelector("#btn").addEventListener("click", () => {
+  /// panaikiname #message elementa
+  document.querySelector("#message").remove();
+  /// fetchiname duomenis is api
+  fetch("https://api.github.com/users")
+    .then((res) => res.json())
+    .then((users) => {
+      /// iteruojam per duomenu masyva
+      users.forEach((user) => {
+        /// kuriame userio kortele
+        const userCard = document.createElement("div");
+        userCard.classList.add('userCard')
+
+        const userLogin = document.createElement("h2");
+        const userLoginTxt = document.createTextNode(user.login);
+        userLogin.appendChild(userLoginTxt);
+
+        const userImg = document.createElement("img");
+        userImg.setAttribute("src", user.avatar_url);
+        userImg.setAttribute("alt", user.login);
+
+        userCard.append(userLogin, userImg);
+        /// atvaizduojame userio kortele ekrane
+        document.querySelector("#output").appendChild(userCard);
+      });
+    });
+});
