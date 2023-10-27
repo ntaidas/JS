839554fb83959abe8e4eafbc81e8b75f372444b0
AidasD <ntaidas@gmail.com>
2023-10-27 10:43:48 +0300

    penkta uzduotis baigta

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,14 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+  
+function filterDogOwners(masyvas){
+   return masyvas.filter(user => user.hasDog === true);  
+}
+
+function filterAdults(masyvas){
+  return masyvas.filter(user => user.age >= 18);
+}
+
+console.log(filterDogOwners(users))
+console.log(filterAdults(users))
