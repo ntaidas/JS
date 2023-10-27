816520346bf5406ca10d4d419e7f58c3c146445f
AidasD <ntaidas@gmail.com>
2023-10-27 08:44:54 +0300

    pirma uzduotis baigta

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,31 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+ document
+    .querySelector('.wrapper > form')
+    .addEventListener('submit', e => {
+        e.preventDefault();
+        /// paimame user input
+        const kg = e.target.elements.search.valueAsNumber;
+
+        /// konvertuojame svori
+        const lb = kg * 2.2046;
+        const g = kg / 0.0010000;
+        const oz = kg * 35.274;
+
+        /// sukuriame elementus
+        const lbEl = document.createElement('p');
+        const lbTxt = document.createTextNode(`${lb} lb`);
+        lbEl.appendChild(lbTxt);
+        
+        const gEl = document.createElement('p');
+        const gTxt = document.createTextNode(`${g} g`);
+        gEl.appendChild(gTxt);
+
+        const ozEl = document.createElement('p');
+        const ozTxt = document.createTextNode(`${oz} oz`);
+        ozEl.appendChild(ozTxt);
+
+        /// atvaizduojam konvertuota svori pusplapyje
+        document.querySelector('#output').append(lbEl,gEl,ozEl)
+    })
