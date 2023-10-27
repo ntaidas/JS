56101b56a0cbf97ac8c91bc09c3bc93aa7c1cd41
AidasD <ntaidas@gmail.com>
2023-10-27 11:36:15 +0300

    uzduotis 9 baigta

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,18 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+class Movie{
+    constructor(title,director,budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpansive(){
+       return this.budget > 100000000? true:false;
+    }
+}
+
+const filmas1 = new Movie('parazitas','as',1000000000000000000);
+const filmas2 = new Movie('parazitas2','tu',10)
+console.log(filmas1.wasExpansive())
+console.log(filmas2.wasExpansive())
